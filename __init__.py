fdca3e4ef97919a662f6215dba5e538ddb8075b9
Alan <alanscodelog@gmail.com>
2017-12-07 15:08:48 -0300

    0.2 - Fixed _RestrictData Error
    - Made variables dynamic.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
    "name": "Useful Menus Add-on",
    "author": "Alan North",
-   "version": (0, 1),
+   "version": (0, 2),
    "blender": (2, 79, 0),
    "location": "None, see documentation to set shortcuts.",
    "description": "Lists sculpt brushes (sculpt.list_brushes).",
@@ -14,8 +14,9 @@ bl_info = {
 import bpy
 import math
 import ctypes #to send escape key
-from pathlib import Path
+from pathlib import Path #to check custom icons are accessible
 
+#PRIVATE METHODS
 class BrushSet(bpy.types.Operator):
    bl_idname = "useful_menus.private_brush_set"
    bl_label = "Set scullpt brush"
@@ -27,7 +28,9 @@ class BrushSet(bpy.types.Operator):
          ctypes.windll.user32.keybd_event(0x1B) #send escape key to exit dialog
       return {'FINISHED'}
 
-#get list of brush names    
+#HELPER FUNCTIONS
+
+#get list of brush names
 def b_names(): 
    brushlist = []
    index = 0
@@ -45,40 +48,54 @@ def b_types():
          types.append(items.sculpt_tool)
    return types
 
-#get ui scale (not dynamic, script needs to be rerun if changed)
-ui_scale = bpy.context.user_preferences.view.ui_scale
+#CONSTANT VARIABLES
+#TODO make configurable?
+
 #the aspect ratio of the buttons (width to height)
 scale = 6
 #button width in pixels
 button_width = 22 * scale
 
+#PUBLIC METHODS
 #popup (not a pie menu because with more than 20+ brushes it just got uncomfortable)
-class CustomDrawOperator(bpy.types.Operator):
+class MenuBrushList(bpy.types.Operator):
    bl_idname = "useful_menus.list_brushes" #set shortcut to this to acess
    bl_label = "Brush List"
-   #get our lists
-   brush_names = b_names()
-   brush_types = b_types()
    
-   #total number of columns and rows
-   col_num = math.ceil(math.sqrt(len(brush_names)/scale)) #total number of brushes divided by scale (so real pixel width and height are the same, then sqrt to keep popup relatively square
-   row_num = math.ceil(len(brush_names)/col_num)
+#   DYNAMIC VARIABLES
+#   set by invoke so they're dynamic and we don't get access restrict errors when trying to get bpy.data
+
+#   brush_names
+#   brush_types
+#   ui_scale
+#   col_num
+#   row_num
    
    def execute(self, context):
       return {'FINISHED'}
 
    def invoke(self, context, event):
+      #SETTING DYNAMIC VARIABLES
+      #get our lists
+      brush_names = self.brush_names = b_names()
+      brush_types = self.brush_types = b_types()
+      ui_scale = self.ui_scale = context.user_preferences.view.ui_scale
+      #total number of columns and rows
+      col_num = self.col_num = math.ceil(math.sqrt(len(brush_names)/scale)) #total number of brushes divided by scale (so real pixel width and height are the same), then sqrt these groups (that represent a 1:1 area) to keep popup relatively square
+      row_num = self.row_num = math.ceil(len(brush_names)/col_num)
+      
       wm = context.window_manager
-      return wm.invoke_popup(self, width=self.col_num*button_width*ui_scale) 
+      return wm.invoke_popup(self, width=col_num*button_width*ui_scale) 
 
    def draw(self, context):
+      
+      #VARIABLES
       layout = self.layout
       toolsettings = context.tool_settings
-      
-      row_num = self.row_num
-      col_num = self.col_num
       brush_names = self.brush_names
       brush_types = self.brush_types
+      col_num = self.col_num
+      row_num = self.row_num
       
       #TODO different lists depending on context so shortcut can be set once for the general 3D View or different shortcuts for each context
       if context.mode == "SCULPT":
@@ -116,6 +133,6 @@ def register():
 
 def unregister():
    bpy.utils.unregister_module(__name__)
-   
+
 if __name__ == "__main__":
    register()
